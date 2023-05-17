a04d4030fe7019b182499accef0ff775ec02fbb7
Arvinder Singh <sardarchitect@gmail.com>
2023-05-16 21:00:42 -0400

    Add streamlit folder

---
+++ streamlit/streamlit_demo.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
+
+st.dataframe(dataframe.style.highlight_max(axis=0))
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
+
+st.map(map_data)
+
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
