01e00880537bc4103dadee3e32ae0e16aabc6465
Arvinder Singh <sardarchitect@gmail.com>
2023-06-02 19:14:13 -0400

    Update scripts

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,1 +1,1 @@
-python yolov5/train.py --img 640 --batch 16 --epochs 5 --data scripts/dataset.yaml --weights yolov5s.pt --name streetstudy
+python yolov5/train.py --img 640 --batch 32 --epochs 1 --data scripts/dataset.yaml --weights yolov5s.pt --name streetstudy 
